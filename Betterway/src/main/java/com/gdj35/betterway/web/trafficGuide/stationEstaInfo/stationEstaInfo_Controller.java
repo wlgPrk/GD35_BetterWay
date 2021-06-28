0144db4eecaaba37842ea7074060b001212ebc10
김지혜 <poo5198@naver.com>
2021-06-28 14:34:16 +0900

    업데이트

--- Betterway/src/main/java/com/gdj35/betterway/web/trafficGuide/stationEstaInfo/stationEstaInfo_Controller.java
+++ Betterway/src/main/java/com/gdj35/betterway/web/trafficGuide/stationEstaInfo/stationEstaInfo_Controller.java
@@ -1,4 +1,4 @@
-package web.trafficGuide.stationEstaInfo;
+package com.gdj35.betterway.web.trafficGuide.stationEstaInfo;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
