0496117ee8e392b9162efde5e306580dc4ea7d6b
방민규 <pbb0636@gmail.com>
2021-07-05 14:40:31 +0900

    편의시설 컨트롤러,dao,service 연결

--- Betterway/src/main/java/com/gdj35/betterway/web/trafficGuide/stationEstaInfo/Dao/EstaDao.java
+++ Betterway/src/main/java/com/gdj35/betterway/web/trafficGuide/stationEstaInfo/Dao/EstaDao.java
@@ -1,5 +1,12 @@
 package com.gdj35.betterway.web.trafficGuide.stationEstaInfo.Dao;
 
-public class EstaDao {
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class EstaDao implements IEstaDao {
+	@Autowired
+	public SqlSession sqlSession;
 
 }
--- Betterway/src/main/java/com/gdj35/betterway/web/trafficGuide/stationEstaInfo/Service/EstaService.java
+++ Betterway/src/main/java/com/gdj35/betterway/web/trafficGuide/stationEstaInfo/Service/EstaService.java
@@ -1,5 +1,13 @@
 package com.gdj35.betterway.web.trafficGuide.stationEstaInfo.Service;
 
-public class EstaService {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.gdj35.betterway.web.trafficGuide.stationEstaInfo.Dao.IEstaDao;
+
+@Service
+public class EstaService implements IEstaService{
+	@Autowired 
+	public IEstaDao iEstaDao;
 
 }
