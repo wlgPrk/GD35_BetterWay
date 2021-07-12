60727abb30b125ca87fab8acc581d314d7361079
방민규 <pbb0636@gmail.com>
2021-07-12 15:42:30 +0900

    adad

---
+++ Betterway/src/main/java/com/gdj35/betterway/web/main/Dao/IMainDao.java
@@ -0,0 +1,10 @@
+package com.gdj35.betterway.web.main.Dao;
+
+import java.util.HashMap;
+import java.util.List;
+
+public interface IMainDao {
+
+	public List<HashMap<String, String>> SubwayList(HashMap<String, String> params) throws Throwable;
+
+}
---
+++ Betterway/src/main/java/com/gdj35/betterway/web/main/Service/MainService.java
@@ -0,0 +1,22 @@
+package com.gdj35.betterway.web.main.Service;
+
+import java.util.HashMap;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.gdj35.betterway.web.main.Dao.IMainDao;
+
+@Service
+public class MainService implements IMainService {
+	@Autowired
+	public IMainDao iMainDao;
+
+	@Override
+	public List<HashMap<String, String>> SubwayList(HashMap<String, String> params) throws Throwable {
+		// TODO Auto-generated method stub
+		return  iMainDao.SubwayList(params);
+	} 
+
+}
